ff434bed0835869eec3c71f350670392119bda59
Alejandro Garrido Mota <garridomota@gmail.com>
2019-12-05 16:00:00 +0000

    Added PSP and installation of Calico

--- main.tf
+++ main.tf
@@ -36,3 +36,66 @@ locals {
   live_domain    = "cloud-platform.service.justice.gov.uk"
 }
 
+# 
+# Helm
+# 
+
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "tiller" {
+  metadata {
+    name = "tiller"
+  }
+
+  role_ref {
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "tiller"
+    namespace = "kube-system"
+    api_group = ""
+  }
+}
+
+resource "null_resource" "deploy" {
+  depends_on = [
+    kubernetes_service_account.tiller,
+    kubernetes_cluster_role_binding.tiller,
+    null_resource.calico_deploy,
+  ]
+
+  provisioner "local-exec" {
+    command = "helm init --wait --service-account tiller"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "kubectl -n kube-system delete deployment.apps/tiller-deploy service/tiller-deploy"
+  }
+}
+
+
+#
+# Calico
+#
+
+resource "null_resource" "calico_deploy" {
+
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/release-1.5/config/v1.5/calico.yaml"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "kubectl delete -f https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/release-1.5/config/v1.5/calico.yaml"
+  }
+}
