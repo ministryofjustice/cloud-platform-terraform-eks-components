470cdc8afad627b2ab646692ab8d57619fd0e7e6
Alejandro Garrido Mota <garridomota@gmail.com>
2020-04-07 14:13:35 +0100

    Nginx ready to go to prod as a module and as a new version

---
+++ ingress-controller.tf
@@ -0,0 +1,19 @@
+
+#########
+# Nginx #
+#########
+
+module "ingress_controllers" {
+  #source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=0.0.1"
+  source = "/Users/mogaal/workspace/github/ministryofjustice/cloud-platform-terraform-ingress-controller"
+
+  cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
+  is_live_cluster     = false
+
+  # This module requires helm and OPA already deployed
+  dependence_prometheus  = helm_release.prometheus_operator
+  dependence_deploy      = null_resource.deploy
+  dependence_opa         = module.opa.helm_opa_status
+  dependence_certmanager = helm_release.cert-manager
+}
+
