63e58c1fc8e02125b2748204a6552049c34d78da
Alejandro Garrido Mota <garridomota@gmail.com>
2020-04-27 18:18:16 +0100

    Using cloud-platform prometheus module

--- cert-manager.tf
+++ cert-manager.tf
@@ -1,13 +1,13 @@
 
 module "cert_manager" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-certmanager?ref=0.0.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-certmanager?ref=0.0.2"
 
   iam_role_nodes      = data.aws_iam_role.nodes.arn
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
-  hostzone            = lookup(var.cluster_r53_resource_maps, terraform.workspace, [data.aws_route53_zone.selected.zone_id])
+  hostzone            = lookup(var.cluster_r53_resource_maps, terraform.workspace, ["arn:aws:route53:::hostedzone/${data.aws_route53_zone.selected.zone_id}"])
 
   # This module requires helm and OPA already deployed
-  dependence_prometheus = helm_release.prometheus_operator
+  dependence_prometheus = module.monitoring.helm_prometheus_operator_status
   dependence_deploy     = null_resource.deploy
   dependence_opa        = module.opa.helm_opa_status
 
