e508cb8e5fca59614b0586d8d71acadcd1332d44
Imran Awan <imran.awan@gec.io>
2020-05-04 16:23:18 +0100

    bumped opa module to latest

--- opa.tf
+++ opa.tf
@@ -3,7 +3,7 @@
 #######
 
 module "opa" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-opa?ref=opa-valid-hostname"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-opa?ref=0.0.2"
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   enable_invalid_hostname_policy = terraform.workspace == local.live_workspace ? false : true
   dependence_deploy = null_resource.deploy
