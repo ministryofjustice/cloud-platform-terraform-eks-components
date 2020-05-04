4b382d7271f37e39812792abec63417070b07a97
Alejandro Garrido Mota <garridomota@gmail.com>
2020-05-04 18:39:07 +0100

    Added metrics server to monitoring module

--- monitoring.tf
+++ monitoring.tf
@@ -1,6 +1,6 @@
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=master"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=0.2.1"
 
   alertmanager_slack_receivers = var.alertmanager_slack_receivers
   iam_role_nodes               = data.aws_iam_role.nodes.arn
