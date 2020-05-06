c89efd598e015413eee597d2da020f0aa8277b50
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2020-05-06 15:33:45 +0100

    Fix broken hostzone variable

--- external-dns.tf
+++ external-dns.tf
@@ -3,7 +3,7 @@ module "external_dns" {
 
   iam_role_nodes      = data.aws_iam_role.nodes.arn
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
-  hostzone            = lookup(var.cluster_r53_resource_maps, terraform.workspace, [data.aws_route53_zone.selected.zone_id])
+  hostzone            = lookup(var.cluster_r53_resource_maps, terraform.workspace, ["arn:aws:route53:::hostedzone/${data.aws_route53_zone.selected.zone_id}"])
 
   # EKS doesn't use KIAM but it is a requirement for the module.
   dependence_kiam   = ""
